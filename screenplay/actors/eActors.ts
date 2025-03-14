e151e613d71975f956a845a75ffda3e320d055e0
aleksandrs.kr <aleksandrs.kr@yggdrasilgaming.com>
2025-03-14 13:40:28 +0200

    fixed enum for better logging step description in reports

--- screenplay/actors/eActors.ts
+++ screenplay/actors/eActors.ts
@@ -1,5 +1,5 @@
 export enum EActorNames {
-  BrowserActor = "Actor, to interact with WEB UI",
-  RestApiActor = "Actor, to interact with REST API",
-  DbActor = "Actor, to interact with DB",
+  BrowserActor = "Browser",
+  RestApiActor = "REST API",
+  DbActor = "Database",
 }
